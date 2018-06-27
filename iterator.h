0b6d52efbb84297fc7bb614ab2ad64b8d465bdba
liangshaocong <liangshaocong1994@gmail.com>
2018-06-27 14:48:20 +0800

    fix error

--- iterator.h
+++ iterator.h
@@ -11,61 +11,14 @@ struct output_iterator_tag {};
 struct forward_iterator_tag : public input_iterator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
-
-template <typename Category, typename T, typename Distance = ptrdiff_t,
-          typename Pointer = T *, typename Reference = T &>
-struct input_iteator {
-  typedef Category iterator_category;
-  typedef T value_type;
-  typedef Distance difference_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-};
-
-template <typename Category, typename T, typename Distance = ptrdiff_t,
-          typename Pointer = T *, typename Reference = T &>
-struct forward_iteator {
-  typedef Category iterator_category;
-  typedef T value_type;
-  typedef Distance difference_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-};
-
-template <typename Category, typename T, typename Distance = ptrdiff_t,
-          typename Pointer = T *, typename Reference = T &>
-struct bidirectional_iterator {
-  typedef Category iterator_category;
-  typedef T value_type;
-  typedef Distance difference_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-};
-
-template <typename Category, typename T, typename Distance = ptrdiff_t,
-          typename Pointer = T *, typename Reference = T &>
-struct random_access_iterator {
-  typedef Category iterator_category;
-  typedef T value_type;
-  typedef Distance difference_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-};
-
-template <typename Iterator> struct iterator {
-  typedef typename Iterator::iterator_category iterator_category;
-  typedef typename Iterator::value_type value_type;
-  typedef typename Iterator::difference_type difference_type;
-  typedef typename Iterator::pointer pointer;
-  typedef typename Iterator::reference reference;
-};
-
-template <typename T> struct iterator<T *> {
-  typedef random_access_iterator_tag iterator_category;
-  typedef T value_type;
-  typedef ptrdiff_t difference_type;
-  typedef T *pointer;
-  typedef T &reference;
+template <typename Category, typename T, typename Difference = ptrdiff_t,
+          typename Pointer = T *, typename Refernce = T &>
+struct Iterator {
+  typedef Category IteratorCategory;
+  typedef T ValueType;
+  typedef Difference DifferenceType;
+  typedef Pointer PointerType;
+  typedef Refernce ReferenceType;
 };
 
 template <typename Iterator> struct iterator_traits {
